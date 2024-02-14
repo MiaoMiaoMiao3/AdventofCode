e4ba4f1b2b1c9fcd582d68231f3fd8e8dfa508ad
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2024-02-14 13:11:04 -0800

    2022 - Day 9 - Copy template files

---
+++ 2022/Day9/helper.py
@@ -0,0 +1,16 @@
+def part1(input):
+	grid = build_grid(input)
+	result = grid
+
+	return result
+
+# def part2(input):
+
+# 	return result
+
+
+# HELPER FUNCTIONS
+
+def build_grid(input):
+	grid = []
+	return grid
