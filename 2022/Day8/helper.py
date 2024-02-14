c4aac32644a66fa9c2c99e9f40753280749a8323
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2024-02-14 12:50:45 -0800

    2022 - Day 8 - debug part 2

--- 2022/Day8/helper.py
+++ 2022/Day8/helper.py
@@ -32,7 +32,7 @@ def find_visible_trees(topography, topography_transposed):
 	for row_idx in range(1, len(topography) - 1):
 		for col_idx in range(1, len(topography[0]) - 1):
 
-			target_tree_height, target_tree_col, target_tree_row = \
+			target_tree_height, target_tree_row, target_tree_col = \
 			find_target_data(topography, topography_transposed, row_idx, col_idx)
 
 			surrounding_tree_heights = find_surrounding_tree_heights_set( \
@@ -47,7 +47,6 @@ def find_target_data(topography, topography_transposed, row_idx, col_idx):
 	target_tree_height = topography[row_idx][col_idx]
 	target_tree_row = topography[row_idx][0:]
 	target_tree_col = topography_transposed[col_idx][0:]
-
 	return target_tree_height, target_tree_row, target_tree_col
 
 def find_surrounding_tree_heights_set(row, col, row_idx, col_idx):
@@ -65,11 +64,10 @@ def find_surrounding_tree_heights_set(row, col, row_idx, col_idx):
 
 def find_surrounding_tree_heights_map(row, col, row_idx, col_idx):
 	surrounding_trees = {}
-	surrounding_trees["above"] = (row[0:col_idx])
-	surrounding_trees["below"] = row[col_idx + 1:len(row)]
-	surrounding_trees["left"] = col[0:row_idx]
-	surrounding_trees["right"] = col[row_idx + 1: len(col)]
-	
+	surrounding_trees["left"] = (row[0:col_idx])
+	surrounding_trees["right"] = row[col_idx + 1:len(row)]
+	surrounding_trees["above"] = col[0:row_idx]
+	surrounding_trees["below"] = col[row_idx + 1: len(col)]
 	return surrounding_trees
 
 def is_visible(surrounding_trees, target_tree_height):
@@ -87,8 +85,9 @@ def find_most_scenic_score(topography, topography_transposed):
 	for row_idx in range(1, len(topography) - 1):
 		for col_idx in range(1, len(topography[0]) - 1):
 
-			target_tree_height, target_tree_col, target_tree_row = \
+			target_tree_height, target_tree_row, target_tree_col = \
 			find_target_data(topography, topography_transposed, row_idx, col_idx)
+
 			surrounding_tree_heights = find_surrounding_tree_heights_map( \
 			target_tree_row, target_tree_col, row_idx, col_idx)
 			target_tree_scenic_score = find_scenic_score(target_tree_height, surrounding_tree_heights)
@@ -99,28 +98,29 @@ def find_most_scenic_score(topography, topography_transposed):
 
 def find_scenic_score(target_tree_height, surrounding_tree_heights):
 	scenic_score = 1
-	print(surrounding_tree_heights)
 	for direction, trees in surrounding_tree_heights.items():
-		visible_tree_counter = 0
+		# Need to start search from inside out relative to target tree
 		if direction == "left" or direction == "above":
-			# Need to start search from inside out relative to target tree
-			for tree_idx in range(len(trees) - 1, -1, -1):
-				if trees[tree_idx] < target_tree_height:
-					visible_tree_counter += 1
-				elif trees[tree_idx] == target_tree_height:
-					visible_tree_counter += 1
-					break
-				else:
-					break
+			start_idx = len(trees) - 1
+			end_idx = -1
+			increment = -1
 		else:
-			for tree_idx in range(0, len(trees)):
-				if trees[tree_idx] <= target_tree_height:
-					visible_tree_counter += 1
-				elif trees[tree_idx] == target_tree_height:
-					visible_tree_counter += 1
-					break
-				else:
-					break
-		# print(tree_idx, direction, target_tree_height, scenic_score, visible_tree_counter)
+			start_idx = 0
+			end_idx = len(trees)
+			increment = 1
+
+		visible_tree_counter = find_last_visible_tree( \
+		trees, start_idx, end_idx, increment, target_tree_height)
+
 		scenic_score *= visible_tree_counter
 	return scenic_score
+
+def find_last_visible_tree(trees, start_idx, end_idx, increment, target_tree_height):
+	visible_tree_counter = 0
+	for tree_idx in range(start_idx, end_idx, increment):
+		if trees[tree_idx] < target_tree_height:
+			visible_tree_counter += 1
+		else:
+			visible_tree_counter += 1
+			break
+	return visible_tree_counter
